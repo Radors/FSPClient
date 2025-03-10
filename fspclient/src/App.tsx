faa2fd7a817d58d268eb0291c3a2744f15e53b48
Samuel Bjelkemyr <samuel.bjelkemyr@outlook.com>
2025-03-10 22:02:31 +0100

    Fully functional MVP

--- fspclient/src/App.tsx
+++ fspclient/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react" 
-import "./App.css"
+import "./css/App.css"
 import TopBar from "./components/TopBar"
 import Soundtrack from "./components/Soundtrack"
 
@@ -9,8 +9,9 @@ function App() {
         <div>
             <div className="container-of-content">
                 <TopBar />
-                <Soundtrack title={"7B Reign"} image={"reign_image.png"} />
-                <Soundtrack title={"8N Forts"} image={"forts_image.png"} />
+                <Soundtrack title={"No. 7 - Reign"} image={"reign_image.png"} audio={"reign_audio.wav"} />
+                <Soundtrack title={"No. 8 - Forts"} image={"forts_image.png"} audio={"forts_audio.wav"} />
+                <Soundtrack title={"No. 9 - Spare"} image={"forts_image.png"} audio={"spare_audio.wav"} />
             </div>
         </div>
     )
--- fspclient/src/components/Soundtrack.tsx
+++ fspclient/src/components/Soundtrack.tsx
@@ -1,9 +1,17 @@
-export default function Soundtrack({ title, image }: {title: string, image: string}) {
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "../css/AudioPlayer.css";
+
+export default function Soundtrack({ title, image, audio }: { title: string, image: string, audio: string }) {
 
     return (
         <div className="soundtrack">
             <img className="soundtrack-image" src={`/src/assets/images/${image}`} />
             <div>{title}</div>
+            <AudioPlayer className="audio-player"
+                src={`/src/assets/audio/${audio}`}
+                showJumpControls={false}
+            />
         </div>
     )
 }

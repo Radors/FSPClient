204e4c63df1a0b9c9adb266d753ddc1b7272e721
Samuel Bjelkemyr <samuel.bjelkemyr@outlook.com>
2025-03-11 18:22:41 +0100

    Added production-intended songs and images

--- fspclient/src/App.tsx
+++ fspclient/src/App.tsx
@@ -9,9 +9,9 @@ function App() {
         <div>
             <div className="container-of-content">
                 <TopBar />
-                <Soundtrack title={"No. 7 - Reign"} image={"reign_image.png"} audio={"reign_audio.wav"} />
-                <Soundtrack title={"No. 8 - Forts"} image={"forts_image.png"} audio={"forts_audio.wav"} />
-                <Soundtrack title={"No. 9 - Spare"} image={"forts_image.png"} audio={"spare_audio.wav"} />
+                <Soundtrack title={"No. 7 - Reign"} image={"reign.png"} audio={"reign.wav"} />
+                <Soundtrack title={"No. 8 - Forts"} image={"forts.png"} audio={"forts.wav"} />
+                <Soundtrack title={"No. 11 - Drift"} image={"drift.png"} audio={"drift.wav"} />
             </div>
         </div>
     )

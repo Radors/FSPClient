a54011aad596f5a771b1b4276eae0fc8b8b29602
Samuel Bjelkemyr <samuel.bjelkemyr@outlook.com>
2025-03-12 14:17:10 +0100

    Add size efficient mp3 files encoded at 320kbps

--- fspclient/src/App.tsx
+++ fspclient/src/App.tsx
@@ -23,9 +23,9 @@ function App() {
         <div>
             <div className="container-of-content">
                 <TopBar />
-                <Soundtrack title={"No. 7 - Reign"} image={"reign.png"} audio={"reign.wav"} reference={ref1} pauseOthers={OnPauseOthers} />
-                <Soundtrack title={"No. 8 - Forts"} image={"forts.png"} audio={"forts.wav"} reference={ref2} pauseOthers={OnPauseOthers} />
-                <Soundtrack title={"No. 11 - Stonk"} image={"stonk.png"} audio={"stonk.wav"} reference={ref3} pauseOthers={OnPauseOthers} />
+                <Soundtrack title={"No. 7 - Reign"} image={"reign.png"} audio={"reign.mp3"} reference={ref1} pauseOthers={OnPauseOthers} />
+                <Soundtrack title={"No. 8 - Forts"} image={"forts.png"} audio={"forts.mp3"} reference={ref2} pauseOthers={OnPauseOthers} />
+                <Soundtrack title={"No. 11 - Stray"} image={"stray.png"} audio={"stray.mp3"} reference={ref3} pauseOthers={OnPauseOthers} />
             </div>
         </div>
     )

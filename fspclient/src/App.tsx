7532bdcabe07a584dabb9cae29b7c1937335d0b7
Samuel Bjelkemyr <samuel.bjelkemyr@outlook.com>
2025-03-16 14:54:34 +0100

    Add new piece, No.14, renamed pieces and swapped images between them

--- fspclient/src/App.tsx
+++ fspclient/src/App.tsx
@@ -23,9 +23,9 @@ function App() {
         <div>
             <div className="container-of-content">
                 <TopBar />
-                <Soundtrack title={"No. 7 - Reign"} image={"reign.png"} audio={"reign.mp3"} reference={ref1} pauseOthers={OnPauseOthers} />
-                <Soundtrack title={"No. 8 - Forts"} image={"forts.png"} audio={"forts.mp3"} reference={ref2} pauseOthers={OnPauseOthers} />
+                <Soundtrack title={"No. 8 - Form"} image={"form.png"} audio={"form.mp3"} reference={ref1} pauseOthers={OnPauseOthers} />
                 <Soundtrack title={"No. 11 - Stray"} image={"stray.png"} audio={"stray.mp3"} reference={ref3} pauseOthers={OnPauseOthers} />
+                <Soundtrack title={"No. 14 - Reign"} image={"reign.png"} audio={"reign.mp3"} reference={ref2} pauseOthers={OnPauseOthers} />
             </div>
         </div>
     )
